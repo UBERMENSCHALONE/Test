15b5954d94435acbe09c520c66d4f4806dfd2568
UBERMENSCHALONE <ubermenschalone@gmail.com>
2020-08-31 14:00:46 +0500

    Initial commit

---
+++ app/src/main/java/com/evilgeniuses/vulgaritytest/activities/ResultActivity.java
@@ -0,0 +1,14 @@
+package com.evilgeniuses.vulgaritytest.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
